6f4b4166b5d3b1a4761150b2fc29c2a47bd5c050
Jerson Suaza <jerson.suaza@gmail.com>
2020-07-04 10:15:33 -0500

    feature:add home view

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Gesture, GestureController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor(private gestureCtrl: GestureController) { }
 
   ngOnInit() {
+    this.initGesture();
+  }
+
+  private initGesture() {
+    const gesture: Gesture = this.gestureCtrl.create({
+      el: document.querySelector('#view-cat'),
+      threshold: 15,
+      gestureName: 'my-gesture',
+      onEnd: ev => this.onEndHandler(ev)
+    }, true);
+    gesture.enable();
+  }
+
+  private onEndHandler(detail) {
+    const type = detail.type;
+    const currentX = detail.currentX;
+    const deltaX = detail.deltaX;
+    const velocityX = detail.velocityX;
+
+    console.log(`deltaX ${deltaX} type ${type} currentx ${currentX} velocityX ${velocityX}`);
+    if (deltaX < 0) {
+      console.log('mover a la izquierda')
+    } else {
+      console.log('mover a la derecha')
+    }
   }
 
 }
